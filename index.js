cd7762a5578a5930f4dd0bac8dd5306dd778eba3
Tamanna Bajaj <tamannabajaj@SIGU-AP-24-N-19.local>
2026-02-26 18:09:26 +0530

    Add detailed comments to CI/CD workflow for PM understanding

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.handler = async () => {
   return {
     statusCode: 200,
-    body: "Hello from Lambda automated deployment!"
+    body: "Hello from Lambda automated deployment evveything!"
   };
 };
